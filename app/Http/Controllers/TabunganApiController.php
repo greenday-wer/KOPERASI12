223771c88cb36d859d27037183386e0ac4495c35
Bayu create <114034586+greenday-wer@users.noreply.github.com>
2024-01-01 18:11:02 +0800

    Update TabunganApiController.php

--- app/Http/Controllers/TabunganApiController.php
+++ app/Http/Controllers/TabunganApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 //use App\Http\Controllers\Controller;
-use App\Models\Tabungan;
+use App\Models\tabungan;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
